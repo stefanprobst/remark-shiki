9a83b2a953b9ae5cfb615fab88fde78732124438
lask <lakscastro@gmail.com>
2022-02-01 16:31:07 -0300

    Add `options` check

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import { visit } from 'unist-util-visit'
 
+const MISSING_HIGHLIGHTER = `Please, provide a valid configuration: .use(remarkShiki, { /* Missing configuration */ highlighter })\n\nCheck \`README.md\` for details`;
+
 export default function attacher(options) {
+  if (!options?.highlighter) throw MISSING_HIGHLIGHTER;
+  
   const highlighter = options.highlighter
   const loadedLanguages = highlighter.getLoadedLanguages()
   const ignoreUnknownLanguage =
